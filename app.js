cdf9873643dcc9faa4d4e3649357b304dffd9d53
System Administrator <root@revanths-mbp.raleigh.ibm.com>
2016-06-28 10:27:52 -0400

    initial commit

--- app.js
+++ app.js
@@ -26,3 +26,66 @@ app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
+
+/*
+	Start of Changes
+*/
+//require('cf-deployment-tracker-client').track();
+var db2;
+var hasString;
+app.set('port', process.env.PORT || 3000);
+
+
+if (process.env.VCAP_SERVICES) {
+    var env = JSON.parse(process.env.VCAP_SERVICES);
+	if (env['dashDB']) {
+		hasString = true;
+        db2 = env['dashDB'][0].credentials;
+        console.log(db2);
+        var connString = "DRIVER={DB2};DATABASE=" + db2.db + ";UID=" + db2.username + ";PWD=" + db2.password + ";HOSTNAME=" + db2.hostname + ";port=" + db2.port;
+	}
+	
+}
+
+
+
+/*
+	Start of Changes
+*/
+
+var ibmdb = require('ibm_db');
+
+
+if(!hasString){
+	global.dbConnString = "DATABASE=BLUDB;HOSTNAME=dashdb-entry-yp-dal09-07.services.dal.bluemix.net;PORT=50000;PROTOCOL=TCPIP;UID=dash6885;PWD=L6FlGJACstKJ;";
+}
+else{
+	global.dbConnString = connString;
+}
+
+app.get('/select', function(req,res){
+	ibmdb.open(dbConnString,function(err,conn){
+	if(err){
+		console.error("Error: ",err);
+		return;
+
+	}else{
+		var query = "SELECT * FROM OFFERING_TABLE";
+		conn.query(query,function(err,rows){
+			if(err){
+				console.error("Error: ",err);
+				return;
+
+			}else{
+				res.send(rows);
+				conn.close(function(){
+					console.log("Connection closed successfully");
+				});
+			}
+
+		});
+	}
+
+});
+
+});
