3af773a617086d12c41d708a9ecc3bef4ba7d118
Sudip Lama <slama@us.ibm.com>
2016-06-30 10:08:41 -0400

    Minor changes to Profile section

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -22,7 +22,7 @@ $(document).ready(function(){
         $.each(data, function(){
             tableContent += '<li class="list-group-item text-right"><a href="/getOfferings/'+ this.OFFERING_ID+'" class="pull-left" ';
             tableContent +='title="'+this.OFFERING_DESCRIPTION+'" >';
-            tableContent += this.OFFERING_CATEGORY + '</a>' + this.CURRENT_LIST_PRICE + '</li>';
+            tableContent += this.OFFERING_CATEGORY + '</a>$ ' + this.CURRENT_LIST_PRICE + '</li>';
         });
         $("div#spinner").hide();
         // Inject the whole content string into our existing HTML table
