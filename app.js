41284f595c39c01c2a326d5b0e54cefd52c16ac4
Sudip Lama <slama@us.ibm.com>
2016-07-06 14:54:14 -0400

    Initial Stage

--- app.js
+++ app.js
@@ -11,206 +11,226 @@ var express = require('express');
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
-
+var fs = require('fs');
 // create a new express server
 var app = express();
 
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
+app.listen(3000);
+var Ibc1 = require('ibm-blockchain-js');
+var ibc = new Ibc1();
 
-// get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
-
-// start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
-  // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
-});
-
-
-var routes = require('./routes');
-
-/*
-	Start of Changes
-*/
-//require('cf-deployment-tracker-client').track();
-var db2;
-var hasString;
-app.set('port', process.env.PORT || 3000);
-
-
-if (process.env.VCAP_SERVICES) {
-    var env = JSON.parse(process.env.VCAP_SERVICES);
-	if (env['dashDB']) {
-		hasString = true;
-        db2 = env['dashDB'][0].credentials;
-        console.log(db2);
-        var connString = "DRIVER={DB2};DATABASE=" + db2.db + ";UID=" + db2.username + ";PWD=" + db2.password + ";HOSTNAME=" + db2.hostname + ";port=" + db2.port;
-	}
-
+// ==================================
+// load peers manually or from VCAP, VCAP will overwrite hardcoded list!
+// ==================================
+//this hard coded list is intentionaly left here, feel free to use it when initially starting out
+//please create your own network when you are up and running
+try{
+	var manual = JSON.parse(fs.readFileSync('mycreds.json', 'utf8'));
+	var peers = manual.credentials.peers;
+	console.log('loading hardcoded peers');
+	var users = null;																		//users are only found if security is on
+	if(manual.credentials.users) users = manual.credentials.users;
+	console.log('loading hardcoded users');
+}
+catch(e){
+	console.log('Error - could not find hardcoded peers/users, this is okay if running in bluemix');
 }
 
 
-
-/*
-	Start of Changes
-*/
-
-
-var ibmdb = require('ibm_db');
-
-global.dbConnString = "DATABASE=BLUDB;HOSTNAME=dashdb-entry-yp-dal09-07.services.dal.bluemix.net;PORT=50000;PROTOCOL=TCPIP;UID=dash6885;PWD=L6FlGJACstKJ;";
-
-app.get('/getOfferings', function(req,res){
-	ibmdb.open(dbConnString,function(err,conn){
-	if(err){
-		console.error("Error: ",err);
-		return;
-
-	}else{
-
-		var query = "SELECT OFFERING_ID, OFFERING_DESCRIPTION, OFFERING_CATEGORY,CURRENT_LIST_PRICE FROM OFFERING_TABLE";
-
-		conn.query(query,function(err,rows){
-			if(err){
-				console.error("Error: ",err);
-				return;
-
-			}else{
-				res.json(rows);
-				conn.close(function(){
-					console.log("Connection closed successfully");
-				});
+if(process.env.VCAP_SERVICES){															//load from vcap, search for service, 1 of the 3 should be found...
+	var servicesObject = JSON.parse(process.env.VCAP_SERVICES);
+	for(var i in servicesObject){
+		if(i.indexOf('ibm-blockchain') >= 0){											//looks close enough
+			if(servicesObject[i][0].credentials.error){
+				console.log('!\n!\n! Error from Bluemix: \n', servicesObject[i][0].credentials.error, '!\n!\n');
+				peers = null;
+				users = null;
+				process.error = {type: 'network', msg: 'Due to overwhelming demand the IBM Blockchain Network service is at maximum capacity.  Please try recreating this service at a later date.'};
 			}
-
-		});
-	}
-
-});
-
-});
-/*
-Get Offering description of paarticular offering
-*/
-
-app.get('/getOfferings/:OFFERING_ID', function(req,res){
-
-	ibmdb.open(dbConnString,function(err,conn){
-	if(err){
-		console.error("Error: ",err);
-		return;
-
-	}else{
-		var query = "SELECT * FROM OFFERING_TABLE WHERE OFFERING_ID = '"+req.params.OFFERING_ID+"' LIMIT 1 ";
-		conn.query(query,function(err,result){
-			if(err){
-				console.error("Error: ",err);
-				return;
-
-			}else{
-				res.json(result);
-				conn.close(function(){
-					console.log("Connection closed successfully");
-				});
-			}
-
-		});
-	}
-
-});
-
-});
-
-/*
-	Get offering category and description
-*/
-app.get('/getOfferingCategory', function(req,res){
-	ibmdb.open(dbConnString,function(err,conn){
-	if(err){
-		console.error("Error: ",err);
-		return;
-
-	}else{
-		var query = "SELECT OFFERING_CATEGORY,OFFERING_DESCRIPTION FROM OFFERING_TABLE";
-		conn.query(query,function(err,rows){
-			if(err){
-				console.error("Error: ",err);
-				return;
-
-			}else{
-				res.send(rows);
-				conn.close(function(){
-					console.log("Connection closed successfully");
-				});
-			}
-
-		});
-	}
-
-});
-
-});
-
-/*
-	Periodic polling function for offering updates.
-	Set to query for updates every 15 secs.
-*/
-
-setInterval(function(){
-		console.log('test');
-
-		ibmdb.open(dbConnString, function(err, conn) {
-
-			if (err ) {
-
-			 console.log("error occurred - Unable to establish connection to db "+err.message);
-			}
-			else {
-				conn.query("SELECT Offering_ID,Current_List_Price from OFFERING_UPDATES WHERE NEW_UPDATE= 'Y'", function(err, rows) {
-
-				if ( !err ) {
-					for (var i = 0; i < rows.length; i++) {
-	  							console.log(rows[i].OFFERING_ID + " ::: " +rows[i].CURRENT_LIST_PRICE );
-
-					/*
-				//	Implement the API call logic
-
-						var options = {
-							  host: 'www.example.com',
-							  port: 80,
-							  path: '/resource?id='+rows[0].OFFERING_ID+'&bar='+rows[0].CURRENT_LIST_PRICE,
-							  method: 'POST'
-						};
-
-						http.request(options, function(res) {
-								console.log('STATUS: ' + res.statusCode);
-								console.log('HEADERS: ' + JSON.stringify(res.headers));
-								res.setEncoding('utf8');
-								res.on('data', function (chunk) {
-								console.log('BODY: ' + chunk);
-								});
-						}).end();
-					*/
-
-					};
-
-				} else {
-				   console.log("error occurred while querying for updates "+err.message);
+			if(servicesObject[i][0].credentials && servicesObject[i][0].credentials.peers){
+				console.log('overwritting peers, loading from a vcap service: ', i);
+				peers = servicesObject[i][0].credentials.peers;
+				if(servicesObject[i][0].credentials.users){
+					console.log('overwritting users, loading from a vcap service: ', i);
+					users = servicesObject[i][0].credentials.users;
 				}
-				});
-
-				conn.query("UPDATE OFFERING_UPDATES SET NEW_UPDATE='N' WHERE NEW_UPDATE='Y'", function(err, rows) {
-				if ( err ) {
-
-				   console.log("error occurred while updating the table"+err.message);
-				}
-
-
-			});
+				else users = null;														//no security
+				break;
 			}
-						conn.close(function(){
-						console.log("Connection Closed");
-					});
-		});
+		}
+	}
+}
 
-}, 15 * 1000);
+// ==================================
+// configure ibm-blockchain-js sdk
+// ==================================
+var options = 	{
+					network:{
+						peers: peers,
+						users: users,
+						options: {quiet: true, tls:false, maxRetry: 1}
+					},
+					chaincode:{
+						/* Custom helloworld
+						zip_url: 'https://github.com/sudip-lama/hellobc/archive/master.zip',
+						unzip_dir: 'hellobc-master/finished',								//subdirectroy name of chaincode after unzipped
+						git_url: 'https://github.com/sudip-lama/hellobc/finished',		//GO get http url
+						*/
+						zip_url: 'https://github.com/sudip-lama/ProductBC/archive/master.zip',
+						unzip_dir: 'ProductBC-master/finished',								//subdirectroy name of chaincode after unzipped
+						git_url: 'https://github.com/sudip-lama/ProductBC/finished',		//GO get http url
+
+
+						//hashed cc name from prev deployment
+						//deployed_name: '14b711be6f0d00b190ea26ca48c22234d93996b6e625a4b108a7bbbde064edf0179527f30df238d61b66246fe1908005caa5204dd73488269c8999276719ca8b'
+					}
+				};
+if(process.env.VCAP_SERVICES){
+	console.log('\n[!] looks like you are in bluemix, I am going to clear out the deploy_name so that it deploys new cc.\n[!] hope that is ok budddy\n');
+	options.chaincode.deployed_name = '';
+}
+ibc.load(options, cb_ready);																//parse/load chaincode
+
+var chaincode = null;
+function cb_ready(err, cc){																	//response has chaincode functions
+	if(err != null){
+		console.log('! looks like an error loading the chaincode or network, app will fail\n', err);
+		if(!process.error) process.error = {type: 'load', msg: err.details};				//if it already exist, keep the last error
+	}
+	else{
+		chaincode = cc;
+
+		if(!cc.details.deployed_name || cc.details.deployed_name === ''){					//decide if i need to deploy
+			cc.deploy('init', ['99'], null, cb_deployed);
+		}
+		else{
+			console.log('chaincode summary file indicates chaincode has been previously deployed');
+			cb_deployed();
+		}
+	}
+}
+
+function cb_got_helloworld(e, data){
+  if(e != null) console.log('[ error] did not get message:', e);
+  else {
+    try{
+    console.log('Blockchain message:'+ JSON.stringify(data));
+    }
+    catch(e){}
+  }
+}
+
+// ============================================================================================================================
+// 												WebSocket Communication Madness
+// ============================================================================================================================
+function cb_deployed(e, d){
+	if(e != null){
+		//look at tutorial_part1.md in the trouble shooting section for help
+		console.log('! looks like a deploy error, holding off on the starting the socket\n', e);
+		if(!process.error) process.error = {type: 'deploy', msg: e.details};
+	}
+	else{
+		console.log('------------------------------------------ Chain is up and running ------------------------------------------');
+		//chaincode.query.read(['_productindex'], cb_got_helloworld);
+		chaincode.query.read(['TS001'], function(err, data){
+        console.log('read abc:', data, err);
+    });
+			//chaincode.query.read_product_index(['test'], cb_got_index);
+	//	chaincode.query.read(['TS002'], cb_got_helloworld);
+/*
+		chaincode.invoke.init_product([
+                "TS001",
+                "Watson",
+                "Text to Speech 01",
+                "6/1/16",
+                "6/1/18",
+                "1000",
+                "USD",
+                "6/2/16",
+                "8/15/16",
+                "product_manager"
+
+            ]
+,write_complete);
+*/
+
+	}
+}
+//got the marble index, lets get each marble
+function cb_got_index(e, index){
+	if(e != null) console.log('[ws error] did not get marble index:', e);
+	else{
+		try{
+			var json = JSON.parse(index);
+			var keys = Object.keys(json);
+			var concurrency = 1;
+			console.log("Object Received:"+JSON.stringify(json));
+			/*
+			//serialized version
+			async.eachLimit(keys, concurrency, function(key, cb) {
+				console.log('!', json[key]);
+				chaincode.query.read([json[key]], function(e, marble) {
+					if(e != null) console.log('[ws error] did not get marble:', e);
+					else {
+						if(marble) sendMsg({msg: 'marbles', e: e, marble: JSON.parse(marble)});
+						cb(null);
+					}
+				});
+			}, function() {
+				sendMsg({msg: 'action', e: e, status: 'finished'});
+			});*/
+		}
+		catch(e){
+			console.log('[ws error] could not parse response', e);
+		}
+	}
+}
+
+function write_complete(e, data){
+  if(e != null) console.log('[ error] did not get message:', e);
+  else {
+    try{
+    chaincode.query.read(['_productindex'], cb_got_helloworld);
+    }
+    catch(e){}
+  }
+}
+app.get('/',function(req, res, next){
+	  res.render('index.html');
+});
+
+app.get('/getProductsList', function(req,res){
+	console.log("Getting ProductList: ")
+	chaincode.query.read(['_productindex'], function(e,index){
+
+		if(e != null) console.log('[ error] did not get message:', e);
+		else {
+			try{
+			console.log('Blockchain message:', JSON.parse(data));
+			//res.json(data);
+			var products=[];
+			var json = JSON.parse(index);
+
+			/*
+			for(var i in json){
+				console.log('!', i, json[i]);
+				chaincode.query.read([json[i]], function(e,data){
+
+					if(e != null) console.log('[ws error] did not get marble:', e);
+					else {
+						try{
+
+						}
+						catch(e){}
+					}
+				});
+			}*/
+		}
+			catch(e){}
+		}
+  	res.send("getProductsList called");
+
+	});
+});
